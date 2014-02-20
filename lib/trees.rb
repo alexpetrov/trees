11d20812b00622edda33d22a8173e0237b375ca2
Alexander Petrov <alexpetrov.rb@gmail.com>
2014-02-21 01:06:34 +0400

    Add method for representing tree as a string.

--- lib/trees.rb
+++ lib/trees.rb
@@ -8,5 +8,17 @@ module Trees
       @value = Array(value)
       @nodes = Array(nodes)
     end
+
+    def self.to_string(node, str = "")
+      return str if node == nil
+      str << '[' << node.value.join(' ')
+      str << ' ' unless node.nodes.empty?
+
+      node.nodes.each do |child|
+        str = to_string(child, str)
+      end
+
+      str << "]"
+    end
   end
 end
--- test/tree_test.rb
+++ test/tree_test.rb
@@ -19,5 +19,16 @@ module Trees
       node = Node.new(1, Node.new(2))
       assert_equal "[2]", node.nodes[0].value.to_s
     end
+
+    def test_one_node_to_s
+      node = Node.new([1, 2])
+      assert_equal "[1 2]", Node::to_string(node)
+    end
+
+    def test_two_nodes_to_s
+      node = Node.new([1, 2], Node.new([3]))
+      assert_equal "[1 2 [3]]", Node::to_string(node)
+    end
+
   end
 end
