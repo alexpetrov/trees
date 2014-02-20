9192eab66db9294d1ff64022aacc4b4e9b75634d
Alexander Petrov <alexpetrov.rb@gmail.com>
2014-02-21 00:01:44 +0400

    Put test class into the same module

--- test/tree_test.rb
+++ test/tree_test.rb
@@ -1,21 +1,23 @@
 # trees/test/tree_test.rb
 require_relative "test_helper"
 
-class TreesTest < Test::Unit::TestCase
+module Trees
 
-  def test_node_class
-    node = Trees::Node.new([1])
-    assert node
+  class TreesTest < Test::Unit::TestCase
+
+    def test_node_class
+      node = Node.new([1])
+      assert node
+    end
+
+    def test_attr_accessors
+      node = Node.new(1)
+      assert_equal "[1]", node.value.to_s
+    end
+
+    def test_nodes_are_arraified_in_constructor
+      node = Node.new(1, Node.new(2))
+      assert_equal "[2]", node.nodes[0].value.to_s
+    end
   end
-
-  def test_attr_accessors
-    node = Trees::Node.new(1)
-    assert_equal "[1]", node.value.to_s
-  end
-
-  def test_nodes_are_arraified_in_constructor
-    node = Trees::Node.new(1, Trees::Node.new(2))
-    assert_equal "[2]", node.nodes[0].value.to_s
-  end
-
 end
