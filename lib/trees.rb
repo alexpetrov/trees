44640e0886c191b115c603bdddbf8d4e485afb53
Alexander Petrov <alexpetrov.rb@gmail.com>
2014-02-21 02:01:08 +0400

    add function that is applies to node's value as a parameter, wich is by default uses join with space

--- lib/trees.rb
+++ lib/trees.rb
@@ -9,9 +9,9 @@ module Trees
       @children = Array(children)
     end
 
-    def self.as_string(node, str = "")
+    def self.as_string(node, str = "", func = ->(x) {x.join(' ')})
       return str if node == nil
-      str << '[' << node.value.join(' ')
+      str << '[' << func.call(node.value)
       str << ' ' unless node.children.empty?
 
       node.children.each do |child|
